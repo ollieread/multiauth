6f548a9e228c664ee77658a810439f97d211a261
Ramon Ackermann <r.ackermann@uselab.com>
2015-03-18 16:13:50 +0100

    Laravel 5 compatibility

--- src/Ollieread/Multiauth/MultiauthServiceProvider.php
+++ src/Ollieread/Multiauth/MultiauthServiceProvider.php
@@ -8,9 +8,18 @@ class MultiauthServiceProvider extends ServiceProvider
 
     protected $defer = false;
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->app->bindShared('auth', function ($app) {
+        $this->app->singleton('auth', function($app)
+        {
+            // Once the authentication service has actually been requested by the developer
+            // we will set a variable in the application indicating such. This helps us
+            // know that we need to set any queued cookies in the after event later.
             $app['auth.loaded'] = true;
 
             return new MultiManager($app);
